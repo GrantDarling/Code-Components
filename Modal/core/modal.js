60162ee98ff0130cdc4beebced84a7b286e54eb3
Grant Darling <grantcarterdarling@gmail.com>
2021-04-16 18:19:55 -0400

    Added sessionStorage functionality to toggle modal

--- Modal/core/modal.js
+++ Modal/core/modal.js
@@ -1,11 +1,24 @@
 /* Modal Core Functionality */
 
-const closeModal = () => {
-    const ModalContainer = document.querySelector("#Modal-backdrop")
-    const HideModal = ModalContainer.style.display = 'none';
+(function () {
+    let modalStatus = sessionStorage.getItem('toggleModal');
+    const ModalContainer = document.querySelector("#Modal-backdrop");
+    const ModalCloseButton = document.querySelector("#Modal-close");
 
-    return HideModal;
-}
+    const initModal = () => {
+        if(!modalStatus) {
+            toggleModal();
+        }
+    }
 
-const ModalCloseBtn = document.querySelector("#Modal-close").addEventListener("click", closeModal);
+    const toggleModal = () => {
+        let openModal = !modalStatus;
+        ModalContainer.style.display = openModal ? 'flex' : 'none';
+        sessionStorage.setItem('toggleModal', modalStatus);
 
+        return modalStatus = !modalStatus;
+    }
+
+    ModalCloseButton.addEventListener("click", toggleModal);
+    window.onload = initModal;
+})();
